285b7df1742b2b896c6b4156de6d92b75702e358
李贺贺 <lihehe@theduapp.com>
2020-03-19 17:23:16 +0800

    Initial commit

---
+++ Example/WAVideoBox/WAViewController.m
@@ -0,0 +1,29 @@
+//
+//  WAViewController.m
+//  WAVideoBox
+//
+//  Created by 464429017@qq.com on 03/19/2020.
+//  Copyright (c) 2020 464429017@qq.com. All rights reserved.
+//
+
+#import "WAViewController.h"
+
+@interface WAViewController ()
+
+@end
+
+@implementation WAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
